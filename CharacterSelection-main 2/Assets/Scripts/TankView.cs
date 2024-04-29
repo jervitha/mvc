ffd30b977dae6b89f3e3e07340709d7a1c4eb3cf
jervitha <147043738+jervitha@users.noreply.github.com>
2024-04-29 22:35:14 +0530

    movement and rotation of tank

--- CharacterSelection-main 2/Assets/Scripts/TankView.cs
+++ CharacterSelection-main 2/Assets/Scripts/TankView.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class TankView : MonoBehaviour
 {
     private TankController tankController;
-    
+
+    private float movement;
+    private float rotation;
+    public Rigidbody rb;
     void Start()
     {
         
@@ -14,10 +17,25 @@ public class TankView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        if (movement != 0)
+            tankController.Move(movement, 30);
+        if (rotation != 0)
+            tankController.Rotate(rotation, 20);
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
     }
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
     }
 }
