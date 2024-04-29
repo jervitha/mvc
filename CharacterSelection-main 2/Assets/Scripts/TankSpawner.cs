4ede411c29f9bc77b335472ca149f58454111387
jervitha <147043738+jervitha@users.noreply.github.com>
2024-04-29 12:16:43 +0530

    tank spawning and prefab creation of tank

---
+++ CharacterSelection-main 2/Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    public GameObject tankPrefab;
+
+    public void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+}
